873852e6e5a2b322f0e2da3e956661ccd14675a8
Anders Soh <sohjiawei@gmail.com>
2025-01-15 11:38:57 +0800

    integrate navigation

--- composeApp/src/commonMain/kotlin/com/hakkasuru/kopicalculator/KopiApp.kt
+++ composeApp/src/commonMain/kotlin/com/hakkasuru/kopicalculator/KopiApp.kt
@@ -1,9 +1,39 @@
 package com.hakkasuru.kopicalculator
 
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.hakkasuru.kopicalculator.drink.DrinkOptionsScreen
 import com.hakkasuru.kopicalculator.home.HomeScreen
 
 @Composable
-fun KopiApp() {
-    HomeScreen()
+fun KopiApp(
+    navController: NavHostController = rememberNavController()
+) {
+    Scaffold {
+        NavHost(
+            navController = navController,
+            startDestination = AppDestination.HOME.route
+        ) {
+            composable(route = AppDestination.HOME.route) {
+                HomeScreen(
+                    navigateToDrinkOptions = {
+                        navController.navigate(
+                            AppDestination.DRINK_OPTIONS.route,
+                        )
+                    }
+                )
+            }
+            composable(route = AppDestination.DRINK_OPTIONS.route) { navBackStackEntry ->
+                val drinkID = navBackStackEntry.arguments?.getString(DestinationArgs.ARG_ID) ?: ""
+                DrinkOptionsScreen(
+                    drinkID = drinkID,
+                    navigateBack = { navController.popBackStack() }
+                )
+            }
+        }
+    }
 }
