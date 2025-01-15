9dd450e464e29fd6a84170dd44e962ea556ce055
Anders Soh <sohjiawei@gmail.com>
2025-01-15 11:38:42 +0800

    add drink options screen

--- composeApp/src/commonMain/kotlin/com/hakkasuru/kopicalculator/drink/DrinkOptionsScreen.kt
+++ composeApp/src/commonMain/kotlin/com/hakkasuru/kopicalculator/drink/DrinkOptionsScreen.kt
@@ -1,2 +1,23 @@
 package com.hakkasuru.kopicalculator.drink
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import com.hakkasuru.kopicalculator.widgets.AppBar
+
+@Composable
+fun DrinkOptionsScreen(
+    drinkID: String,
+    navigateBack: () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            AppBar(
+                title = "Options",
+                onNavigationIconTap = navigateBack
+            )
+        }
+    ) {
+        Column { }
+    }
+}
