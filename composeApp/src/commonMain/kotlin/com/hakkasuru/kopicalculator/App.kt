3d70bdafb58e424b55ca2ea1b56ab824417e7605
Anders Soh <sohjiawei@gmail.com>
2025-01-15 11:38:27 +0800

    remove unused import

--- composeApp/src/commonMain/kotlin/com/hakkasuru/kopicalculator/App.kt
+++ composeApp/src/commonMain/kotlin/com/hakkasuru/kopicalculator/App.kt
@@ -3,7 +3,6 @@ package com.hakkasuru.kopicalculator
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import com.hakkasuru.kopicalculator.home.HomeScreen
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
