7615dc99f719656763aba664b974e03de58b349a
Anders Soh <sohjiawei@gmail.com>
2025-01-16 11:09:27 +0800

    update route

--- composeApp/src/commonMain/kotlin/com/hakkasuru/kopicalculator/KopiApp.kt
+++ composeApp/src/commonMain/kotlin/com/hakkasuru/kopicalculator/KopiApp.kt
@@ -3,9 +3,11 @@ package com.hakkasuru.kopicalculator
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.hakkasuru.kopicalculator.drink.DrinkOptionsScreen
 import com.hakkasuru.kopicalculator.home.HomeScreen
 
@@ -22,12 +24,22 @@ fun KopiApp(
                 HomeScreen(
                     navigateToDrinkOptions = {
                         navController.navigate(
-                            AppDestination.DRINK_OPTIONS.route,
+                            AppDestination.DRINK_OPTIONS.route
+                                .replace("{${DestinationArgs.ARG_ID}}", it)
                         )
                     }
                 )
             }
-            composable(route = AppDestination.DRINK_OPTIONS.route) { navBackStackEntry ->
+            composable(
+                route = AppDestination.DRINK_OPTIONS.route,
+                arguments = listOf(
+                    navArgument(DestinationArgs.ARG_ID) {
+                        type = NavType.StringType
+                        nullable = false
+                        defaultValue = ""
+                    }
+                )
+            ) { navBackStackEntry ->
                 val drinkID = navBackStackEntry.arguments?.getString(DestinationArgs.ARG_ID) ?: ""
                 DrinkOptionsScreen(
                     drinkID = drinkID,
