53a061c45eb547b977b54c8bfbf070583f4234b6
Anders Soh <sohjiawei@gmail.com>
2025-05-07 16:56:59 +0800

    Refactors: Cleans up unused dependencies
    Removes unused dependencies and cleans up the code by
    removing unused imports and unnecessary code blocks.
    This improves the code's readability and reduces its size.

--- composeApp/src/commonMain/kotlin/com/hakkasuru/kopicalculator/widgets/OptionCard.kt
+++ composeApp/src/commonMain/kotlin/com/hakkasuru/kopicalculator/widgets/OptionCard.kt
@@ -11,13 +11,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
